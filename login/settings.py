d6c1c477935e9b40eeb92eacddd8fcba85ae800c
sagar <sagarkapse121212@gmail.com>
2020-03-19 19:43:35 +0530

    "second commit"

--- login/settings.py
+++ login/settings.py
@@ -120,4 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
 LOGIN_REDIRECT_URL = '/accounts/index/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+MEDIA_ROOT =  os.path.join(BASE_DIR,'static/images')
